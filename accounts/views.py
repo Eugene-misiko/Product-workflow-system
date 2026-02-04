9c3f4e3735e9e7a3b318d0d31fabe0b6ac4ba3bc
Eugene <eugenemisiko438@gmail.com>
2026-02-04 23:21:16 +0300

    creating registration API

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import User, Role
+from .serializers import RegisterSerializer
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+
+    def perform_create(self, serializer):
+        client_role = Role.objects.get(name=Role.CLIENT)
+        serializer.save(role=client_role)
