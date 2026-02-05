0fa3dfbb2ef0beac32debb0e7bca9a686285a3c1
Eugene <eugenemisiko438@gmail.com>
2026-02-05 23:43:16 +0300

    creating views for user notification

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Notification
+from .serializers import NotificationSerializer
 # Create your views here.
+class NotificationViewSet(ModelViewSet):
+    """
+    User notifications.
+    """
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
