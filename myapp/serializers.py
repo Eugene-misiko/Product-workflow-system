fb24b1657ebe7854020f4523c195d8a14cc9f3bd
Eugene <eugenemisiko438@gmail.com>
2026-02-05 21:43:03 +0300

    serializing the products fields by using model serializer for a clean output or code

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,19 +1,12 @@
 from rest_framework import serializers
+from .models import Category, Product
 
-from .models import Book
-
-class BookSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length=150)
-    author = serializers.CharField(max_length=100)
-    description = serializers.CharField()
-    publisher = serializers.CharField(max_length=100)
-    published_date = serializers.DateField(required=False)
-    page_count = serializers.IntegerField(required=False, allow_null=True, min_value=1)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, allow_null=True, required=False)
-    updated_at = serializers.DateTimeField(read_only=True) 
-
-
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Book
+        model = Category
+        fields = "__all__"
 
-        fields = '__all__'   
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
