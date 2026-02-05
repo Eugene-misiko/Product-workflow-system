4d2fa86fc9c32a152c9a87174d296a023f5976a8
Eugene <eugenemisiko438@gmail.com>
2026-02-05 22:44:11 +0300

    creating models for the payments

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from orders.models import Order
 # Create your models here.
+class Payment(models.Model):
+    """Payments for orders"""
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    confirmed = models.BooleanField(default=False)
--- print_flow/settings.py
+++ print_flow/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'accounts',
     'orders',
     'designs',
+    'payments',
 ]
 
 MIDDLEWARE = [
