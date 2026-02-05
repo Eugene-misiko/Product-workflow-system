4ffbba24936710bad1d0b150ab673f6221cb2063
Eugene <eugenemisiko438@gmail.com>
2026-02-05 22:46:49 +0300

    serializing the payments

---
+++ payments/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Payment
+
+class PaymentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for order payments.
+    """
+    class Meta:
+        model = Payment
+        fields = "__all__"
+        read_only_fields = ("confirmed",)
