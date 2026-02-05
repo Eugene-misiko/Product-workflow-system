b4ceb28546b29bebf7de9c181256e91afd0425ca
Eugene <eugenemisiko438@gmail.com>
2026-02-05 23:27:10 +0300

    creating views for delivery note

--- deliveries/views.py
+++ deliveries/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Delivery
+from .serializers import DeliverySerializer
 # Create your views here.
+
+class DeliveryViewSet(ModelViewSet):
+    """
+    Delivery tracking.
+    """
+    queryset = Delivery.objects.all()
+    serializer_class = DeliverySerializer
