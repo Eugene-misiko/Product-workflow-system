eb8c3e408db8b98885991d12dbfeb8d74e824c0e
Eugene <eugenemisiko438@gmail.com>
2026-02-05 21:09:42 +0300

    modifying the permissions for each users

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,28 +1,26 @@
 from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
+    """
+    Allows access only to admin users.
+    """
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and
-            request.user.role and
-            request.user.role.name == "ADMIN"
-        )
+        return request.user.is_authenticated and request.user.role == "admin"
 
 
 class IsClient(BasePermission):
+    """
+    Allows access only to client users.
+    """
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and
-            request.user.role and
-            request.user.role.name == "CLIENT"
-        )
+        return request.user.is_authenticated and request.user.role == "client"
 
 
 class IsDesigner(BasePermission):
+    """
+    Allows access only to designer users.
+    """
     def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated and
-            request.user.role and
-            request.user.role.name == "DESIGNER"
-        )
+        return request.user.is_authenticated and request.user.role == "designer"
+
 
