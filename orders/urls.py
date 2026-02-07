5d11aaf29166918a659d3ad95382f34cbdfe0741
Eugene <eugenemisiko438@gmail.com>
2026-02-07 22:27:11 +0300

    creating templates for order list

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet
-
+from .views import OrderViewSet,order_list_template, order_detail_template
+from django.urls import path
 router = DefaultRouter()
 router.register("orders", OrderViewSet)
 
 
 urlpatterns = router.urls
+urlpatterns += [
+    path("view/orders/", order_list_template, name="order_list_template"),
+    path("view/orders/<int:order_id>/", order_detail_template, name="order_detail_template"),
+]
