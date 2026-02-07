a851e915296506acff07186a84de4cc7239ca79f
Eugene <eugenemisiko438@gmail.com>
2026-02-07 21:45:10 +0300

    creating the template for category list in myapp

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,15 @@
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet
+from .views import CategoryViewSet, ProductViewSet,category_list_template, product_list_template
+from django.urls import path
 
 router = DefaultRouter()
 router.register("categories", CategoryViewSet)
 router.register("myapp", ProductViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("view/categories/", category_list_template, name="category_list_template"),
+    path("view/products/", product_list_template, name="product_list_template"),
+]
 
