a0d4eba2baa7495596ec954befec5d231a1fe03e
Eugene <eugenemisiko438@gmail.com>
2026-02-08 01:15:29 +0300

    creating views for the notification list template

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import NotificationViewSet
-
+from .views import NotificationViewSet, notification_list_template
+from django.urls import path
 router = DefaultRouter()
 router.register("notifications", NotificationViewSet)
 
 urlpatterns = router.urls
+urlpatterns += [
+    path("view/notifications/", notification_list_template, name="notification_list_template"),
+]
