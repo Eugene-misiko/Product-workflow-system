e8b3427a7e9d630807398200f092c94b4d5910fe
Eugene <eugenemisiko438@gmail.com>
2026-02-08 00:56:13 +0300

    creating views for the templates

--- documents/views.py
+++ documents/views.py
@@ -9,3 +9,13 @@ class LegalDocumentViewSet(ModelViewSet):
     """
     queryset = LegalDocument.objects.all()
     serializer_class = LegalDocumentSerializer
+
+def document_list_template(request):
+    """
+    Admin-only: View all legal documents.
+    """
+    if not request.user.is_authenticated or request.user.role != "admin":
+        return render(request, "forbidden.html", status=403)
+
+    documents = LegalDocument.objects.all()
+    return render(request, "document_list.html", {"documents": documents})
