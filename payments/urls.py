462ddee6701ba1b8bcf8cf813d98b429324b149f
Eugene <eugenemisiko438@gmail.com>
2026-02-08 00:26:26 +0300

    creating template for payment list

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet
-
+from .views import PaymentViewSet, payment_list_template
+from django.urls import path
 router = DefaultRouter()
 router.register("payments", PaymentViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("view/payments/", payment_list_template, name="payment_list_template"),
+]
