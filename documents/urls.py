4d1010bda912b544dd6fb107be908a510bf427ab
Eugene <eugenemisiko438@gmail.com>
2026-02-08 00:59:44 +0300

    creating a template for document list

--- documents/urls.py
+++ documents/urls.py
@@ -1,7 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import LegalDocumentViewSet
-
+from .views import LegalDocumentViewSet,document_list_template
+from django.urls import path
 router = DefaultRouter()
 router.register("documents", LegalDocumentViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("view/documents/", document_list_template, name="document_list_template"),
+]
+
