d03e5880441a78287ea97bebf7d213c0d1b9cdf3
Eugene <eugenemisiko438@gmail.com>
2026-02-08 00:48:34 +0300

    creating the template for the design list

--- designs/urls.py
+++ designs/urls.py
@@ -1,8 +1,13 @@
 from rest_framework.routers import DefaultRouter
-from .views import DesignViewSet, DesignRequestViewSet
-
+from .views import DesignViewSet, DesignRequestViewSet, design_list_template, design_request_list_template
+from django.urls import path
 router = DefaultRouter()
 router.register("designs", DesignViewSet)
 router.register("design-requests", DesignRequestViewSet)
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("view/designs/", design_list_template, name="design_list_template"),
+    path("view/design-requests/", design_request_list_template, name="design_request_list_template"),
+]
