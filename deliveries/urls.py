513a482748dfddbb85e2c00c488f8ec6a5dccdbb
Eugene <eugenemisiko438@gmail.com>
2026-02-08 00:19:08 +0300

    creating the delivery template that show the tables

--- deliveries/urls.py
+++ deliveries/urls.py
@@ -1,7 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from .views import DeliveryViewSet
+from .views import DeliveryViewSet,delivery_list_template
+from django.urls import path
 
 router = DefaultRouter()
 router.register("deliveries", DeliveryViewSet)
 
 urlpatterns = router.urls
+urlpatterns += [
+    path("view/deliveries/", delivery_list_template, name="delivery_list_template"),
+]
