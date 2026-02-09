b8680d3ca6a2ebfa693587d77d98a9be54fc7799
Eugene <eugenemisiko438@gmail.com>
2026-02-10 02:25:15 +0300

    Creating models for the auditing

--- audit/models.py
+++ audit/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
+class AuditLog(models.Model):
+    """
+    Sytem audit logs 
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    action = models.CharField(max_length=100)
+    details = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
