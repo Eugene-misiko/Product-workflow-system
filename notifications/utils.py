08175b89ba927b8613c86c009f11627f6869867a
Eugene <eugenemisiko438@gmail.com>
2026-02-12 01:44:31 +0300

    modifying the code in the utils.py

--- notifications/utils.py
+++ notifications/utils.py
@@ -1,56 +1,13 @@
 from .models import Notification
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-def create_notification(user, message):
+def notify(user, message):
     """
-    Create a notification for a single user.
+    Create a notification for a given user.
+    
+    Args:
+        user: User instance who will receive the notification
+        message: String message for the notification
     """
-    if not user:
-        return None
+    if user is not None and message:
+        Notification.objects.create(user=user, message=message)
 
-    return Notification.objects.create(
-        user=user,
-        message=message
-    )
-
-
-def create_bulk_notification(users, message):
-    """
-    Create the same notification for multiple users.
-    """
-    notifications = [
-        Notification(user=user, message=message)
-        for user in users
-    ]
-    return Notification.objects.bulk_create(notifications)
-
-
-def mark_notification_as_read(notification):
-    """
-    Mark a single notification as read.
-    """
-    notification.is_read = True
-    notification.save()
-    return notification
-
-
-def mark_all_as_read(user):
-    """
-    Mark all notifications for a user as read.
-    """
-    return Notification.objects.filter(
-        user=user,
-        is_read=False
-    ).update(is_read=True)
-
-
-def get_unread_count(user):
-    """
-    Return number of unread notifications for a user.
-    """
-    return Notification.objects.filter(
-        user=user,
-        is_read=False
-    ).count()
