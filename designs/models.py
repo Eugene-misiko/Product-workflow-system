3124b11fd3dff804035d371fbc87c4e66755c082
Eugene <eugenemisiko438@gmail.com>
2026-02-14 17:46:08 +0300

    modification of models in the designed app

--- designs/models.py
+++ designs/models.py
@@ -6,10 +6,17 @@ from orders.models import Order
 from accounts.models import User
 
 class Design(models.Model):
-    """Uploaded design file"""
+    """
+    Uploaded design file
+    Represents a design file uploaded by a client.
+    Used when client selects "Already Designed    
+    """
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     file = models.FileField(upload_to="designs/")
     status = models.CharField(max_length=20, default="pending")
+
+    def __str__(self):
+        return f"Design for Order #{self.order.id}"    
 
 class DesignRequest(models.Model):
     """Design work request"""
