58a3c12794bda0dcce251962dbeedf110aa4d21b
Eugene <eugenemisiko438@gmail.com>
2026-02-14 17:51:54 +0300

    modifying dcocuments models

--- documents/models.py
+++ documents/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from orders.models import Order
 # Create your models here.
 class LegalDocument(models.Model):
-    """Generated contract"""
+    """
+    Generated contract
+    Represents generated legal contract document
+    for a specific order.    
+    """
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
     file = models.FileField(upload_to="documents/")
+
+    def __str__(self):
+        return f"Legal Document for Order #{self.order.id}"    
