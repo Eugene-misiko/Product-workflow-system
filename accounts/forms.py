cb0b5e094b7a7e3d0c63509c675e5e68ff541483
Eugene <eugenemisiko438@gmail.com>
2026-02-14 16:25:47 +0300

    styling the notification form

--- accounts/forms.py
+++ accounts/forms.py
@@ -20,5 +20,6 @@ class CustomUserCreationForm(UserCreationForm):
                 'class': 'border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500'}),
             'password2': forms.PasswordInput(attrs={
                 'class': 'border border-gray-300 rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500'}),
-              
+
+
         }
