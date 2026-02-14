c5e49c6c237b565c8614bca10b51bcba364f7b40
Eugene <eugenemisiko438@gmail.com>
2026-02-14 17:41:30 +0300

    modifying deliveries models

--- deliveries/models.py
+++ deliveries/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from orders.models import Order
 # Create your models here.
 class Delivery(models.Model):
-    """Delivery note"""
+    """Delivery note-Represents the delivery information of an order.
+        One order has only one delivery record.
+        Stores the delivery date.
+    """
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
     delivered_at = models.DateField()
+    
+    def __str__(self):
+        return f"Delivery for Order #{self.order.id}"
