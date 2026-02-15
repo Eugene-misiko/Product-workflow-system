eac2addbaf91c2cb1cd147523656a19c11be0a6a
Eugene <eugenemisiko438@gmail.com>
2026-02-15 03:02:58 +0300

    updating the form for the creation form

--- orders/forms.py
+++ orders/forms.py
@@ -3,6 +3,21 @@ from django import forms
 from .models import Order,OrderItem,Product
 from myapp.models import Category
 #modified 
+
+DESIGN_CHOICES = [
+    ("designed", "I have my design"),
+    ("not_designed", "I want it designed"),
+]
+
+PAPER_CHOICES = [
+    ("matte", "Matte"),
+    ("glossy", "Glossy"),
+]
+
+EDIT_CHOICES = [
+    ("simple", "Simple editing"),
+    ("full", "Full editing"),
+]
 class OrderCreateForm(forms.Form):
     """
     Form used by client to create an order.
@@ -12,13 +27,25 @@ class OrderCreateForm(forms.Form):
     2. Select product (filtered by category)
     3. Enter quantity
     """
+    category = forms.ModelChoiceField(queryset=Category.objects.filter(is_active=True))
+    product = forms.ModelChoiceField(queryset=Product.objects.none())  # Initially empty
+    quantity = forms.IntegerField(min_value=1)
+    color = forms.CharField(max_length=50)
+    design_type = forms.ChoiceField(choices=DESIGN_CHOICES)
+    
+    # Fields for not_designed
+    description = forms.CharField(widget=forms.Textarea, required=False)
+    paper_type = forms.ChoiceField(choices=PAPER_CHOICES, required=False)
+    editing_type = forms.ChoiceField(choices=EDIT_CHOICES, required=False)
 
-class OrderCreateForm(forms.ModelForm):
-    category = forms.ModelChoiceField(queryset=Category.objects.filter(is_active=True), required=True)
-    product = forms.ModelChoiceField(queryset=Product.objects.none(), required=True)
-    quantity = forms.IntegerField(min_value=1, required=True)
-    color = forms.CharField(max_length=50, required=True)
+
+
+
+class Item(forms.ModelForm):
+    """
+    creating Item form
+    """
 
     class Meta:
         model = OrderItem
-        fields = ['category', 'product', 'quantity', 'color']
+        fields = '__all__'
