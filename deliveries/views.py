77b10eb79e4eb98a54a71e71e47a7b061f0bbe66
Eugene <eugenemisiko438@gmail.com>
2026-02-16 14:49:58 +0300

    updating the views for creating delivery

--- deliveries/views.py
+++ deliveries/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from rest_framework.viewsets import ModelViewSet
 from .models import Delivery
+from orders.models import Order
+from django.contrib.auth.decorators import login_required
 from .serializers import DeliverySerializer
 # Create your views here.
 
@@ -21,3 +23,31 @@ def delivery_list_template(request):
 
     deliveries = Delivery.objects.all()
     return render(request, "delivery_list.html", {"deliveries": deliveries})
+@login_required
+def create_delivery(request, order_id):
+
+    if request.user.role != "admin":
+        return render(request, "forbidden.html", status=403)
+
+    order = get_object_or_404(Order, id=order_id)
+
+    # Prevent duplicate delivery
+    if hasattr(order, "delivery"):
+        return redirect("order_detail", order_id=order.id)
+
+    if request.method == "POST":
+        address = request.POST.get("address")
+        phone = request.POST.get("phone")
+
+        Delivery.objects.create(
+            order=order,
+            address=address,
+            phone=phone
+        )
+
+        order.status = "on_delivery"
+        order.save()
+
+        return redirect("order_detail", order_id=order.id)
+
+    return render(request, "create_delivery.html", {"order": order})
