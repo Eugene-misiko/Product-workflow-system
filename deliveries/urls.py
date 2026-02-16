31788f9b313b85ca48fffb5747dbc33f629b34ef
Eugene <eugenemisiko438@gmail.com>
2026-02-16 22:58:07 +0300

    creating delivery template to create the delivery by the admin

--- deliveries/urls.py
+++ deliveries/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import DeliveryViewSet,delivery_list_template
+from .views import DeliveryViewSet,delivery_list_template, create_delivery, update_delivery_status
 from django.urls import path
 
 router = DefaultRouter()
@@ -8,4 +8,6 @@ router.register("deliveries", DeliveryViewSet)
 urlpatterns = router.urls
 urlpatterns += [
     path("view/deliveries/", delivery_list_template, name="delivery_list_template"),
+    path("create/<int:order_id>/", create_delivery, name="create_delivery"),
+    path("update/<int:order_id>/", update_delivery_status, name="update_delivery_status"),
 ]
