ce159922916ec8ec24e320b56250a318ade74cfa
Eugene <eugenemisiko438@gmail.com>
2026-02-17 01:31:19 +0300

    creating a form for the payment

--- payments/forms.py
+++ payments/forms.py
@@ -1,17 +1,18 @@
 from django import forms
-from .models import Payment
-
-
+from .models import Payment, MethodPay
 
 class PaymentForm(forms.ModelForm):
-    """
-    Payment creation form.
-    """
+    phone_number = forms.CharField(
+        max_length=15, 
+        required=True,
+        label="Phone Number"
+    )
 
     class Meta:
         model = Payment
-        fields = ["method_of_payment", "amount"]# amount
+        fields = ["method_of_payment", "amount", "phone_number"]
 
-        
-
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Make method_of_payment a dropdown with choices
+        self.fields['method_of_payment'].queryset = MethodPay.objects.all()
--- payments/urls.py
+++ payments/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet, payments_list, payment_create
+from .views import PaymentViewSet,stk_push,mpesa_callback, payments_list, payment_create
 from django.urls import path
 router = DefaultRouter()
 router.register("payments", PaymentViewSet)
@@ -9,4 +9,6 @@ urlpatterns = router.urls
 urlpatterns += [
     path("view/payments/", payments_list, name="payment_list_template"),
     path("view/payments/create/<int:order_id>/", payment_create, name="payment_create"),
+    path("api/stk_push/", stk_push, name="stk_push"),
+    path("api/mpesa_callback/", mpesa_callback, name="mpesa_callback"),    
 ]
