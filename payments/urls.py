37dac4d68103a062d397eefac71e9ba8966b7821
Eugene <eugenemisiko438@gmail.com>
2026-02-17 01:46:47 +0300

    updating orderdetail template for creation of the payment

--- payments/urls.py
+++ payments/urls.py
@@ -1,14 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet,stk_push,mpesa_callback, payments_list, payment_create
+from .views import  mpesa_callback, payments_list, payment_create
 from django.urls import path
-router = DefaultRouter()
-router.register("payments", PaymentViewSet)
 
-urlpatterns = router.urls
 
-urlpatterns += [
+
+
+urlpatterns= [
     path("view/payments/", payments_list, name="payment_list_template"),
     path("view/payments/create/<int:order_id>/", payment_create, name="payment_create"),
-    path("api/stk_push/", stk_push, name="stk_push"),
     path("api/mpesa_callback/", mpesa_callback, name="mpesa_callback"),    
 ]
