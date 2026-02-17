b3b7b96835d99aa67b6f97e0165d996e95b6cba7
Eugene <eugenemisiko438@gmail.com>
2026-02-17 11:26:16 +0300

    creating the form for the category and product

---
+++ myapp/forms.py
@@ -0,0 +1,16 @@
+from .models import Category
+from .models import Product
+from django import forms
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = '__all__'     
+
+
+    
