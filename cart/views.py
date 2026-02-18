df13443a1ca5bd3bd8da0f7b0642e42e5a0c17ed
Eugene <eugenemisiko438@gmail.com>
2026-02-18 20:59:37 +0300

    added models for the cart, and creating views for dynamically update the cart information without requiring a page reload

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from myapp.models import Product
+from .models import Cart, CartItems
+from django.views.decorators import require_POST
 
 # Create your views here.
+@require_POST
+def cart_add(request, product_id):
+    cart_id = request.session.get('cart_id')
+
+    if cart_id:
+        try:
+            cart = Cart.objects.get(id=cart_id)
+        except Cart.DoesNotExist:
+            cart = Cart.objects.create()
+    else:
+        cart = Cart.object.create() 
+        request.session["cart_id"] = cart.id 
+
+    product = get_object_or_404(Product, id=product_id) 
+    cart_item, created = CartItems.objects.get_or_create(cart=cart, product=product)
+
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()   
