77d396d77d61bf5f7097b7e8b2bdf7a775aa970f
Eugene <eugenemisiko438@gmail.com>
2026-02-18 11:22:34 +0300

    creating the models for products and categories

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    class Meta:
+        verbose_name_plural ="categories"
 
 class Product(models.Model):
-    pass
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = CloudinaryField('image')
 
+    def __str__(self):
+        return self.name
+    
 
+    def get_absolute_url(self):
+        return reverse('products:product_detail', kwargs={'id':self.id, 'slug':self.slug})
 
 class Subscriber(models.Model):
     email = models.EmailField(unique=True)
