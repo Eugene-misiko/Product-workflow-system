e1b42f830d0606ac99a3a66b28a8f16e4c5f6f37
Eugene <eugenemisiko438@gmail.com>
2026-02-27 00:37:19 +0300

    creating a new file for the combination of the models to create a dynamic role

---
+++ myapp/utils.py
@@ -0,0 +1,11 @@
+from .models import Card, Banner, Clothes, Book, Flyer, Pen, Envelope
+
+PRODUCT_MODELS = {
+    'card': Card,
+    'banner': Banner,
+    'clothes': Clothes,
+    'book': Book,
+    'flyer': Flyer,
+    'pen': Pen,
+    'envelope': Envelope,
+}
