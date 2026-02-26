67d31577556df48be858450523fd65f69161f35e
Eugene <eugenemisiko438@gmail.com>
2026-02-27 01:35:22 +0300

    serializing the ItemOrder table

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Product, Item_order
-from .utils import PRODUCT_MODELS
+from .models import Category, Product, ItemOrder
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
@@ -11,45 +10,8 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = "__all__"
 
-
-class DynamicOrderSerializer(serializers.ModelSerializer):
+class ItemOrderSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item_order
-        fields = ['id', 'product_type', 'product_id', 'quantity', 'status', 'created_at']
-        read_only_fields = ['status', 'created_at', 'user']
-
-    def validate(self, attrs):
-        product_type = attrs.get('product_type').lower()
-        product_id = attrs.get('product_id')
-        quantity = attrs.get('quantity', 1)
-
-        if product_type not in PRODUCT_MODELS:
-            raise serializers.ValidationError({'product_type': 'Invalid product type'})
-
-        product_model = PRODUCT_MODELS[product_type]
-        try:
-            product = product_model.objects.get(id=product_id)
-        except product_model.DoesNotExist:
-            raise serializers.ValidationError({'product_id': 'Product does not exist'})
-
-        if quantity > product.quantity_available:
-            raise serializers.ValidationError({'quantity': 'Not enough stock available'})
-
-        attrs['product_instance'] = product
-        return attrs
-
-    def create(self, validated_data):
-        user = self.context['request'].user
-        product = validated_data.pop('product_instance')
-        quantity = validated_data['quantity']
-
-        product.quantity_available -= quantity
-        product.save()
-
-        order = Item_order.objects.create(
-            user=user,
-            product_type=validated_data['product_type'],
-            product_id=validated_data['product_id'],
-            quantity=quantity
-        )
-        return order
+        model = ItemOrder
+        fields = '__all__'
+        read_only_fields = ['user', 'status', 'created_at']
