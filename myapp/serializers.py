ae3debb8418d17b3b7a98488ea1c8e0021e0fa01
Eugene <eugenemisiko438@gmail.com>
2026-03-01 18:12:59 +0300

    deleting and updating serializesr

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,21 +1,7 @@
 from rest_framework import serializers
-from .models import Category, Product, ItemOrder
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = "__all__"
+from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Product
         fields = "__all__"
-
-class ItemOrderSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username', read_only=True)
-    user_id = serializers.IntegerField(source='user.id', read_only=True)    
-
-    class Meta:
-        model = ItemOrder
-        fields = '__all__'
-        read_only_fields = ['user', 'status', 'created_at']
