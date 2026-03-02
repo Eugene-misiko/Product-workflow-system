fb63d6032d550dfa6cc49310487452b6c3907812
Eugene <eugenemisiko438@gmail.com>
2026-03-02 11:54:14 +0300

    updating serializwrs

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,47 +1,44 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import User
 
-class RegisterSerializer(serializers.ModelSerializer):
-    """
-    Secure serializer for registering new users.
-    All new users are automatically assigned the CLIENT role.
-    """
 
-    password = serializers.CharField(write_only=True)
+class RegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
 
     class Meta:
         model = User
-        fields = ("username", "email", "phone", "password")
-        extra_kwargs = {
-            "password": {"write_only": True}
-        }
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "phone",
+            "password",
+        )
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already registered")
+        return value
 
     def create(self, validated_data):
-        """
-        Create a new user and force role to CLIENT.
-        """
+
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
+            first_name=validated_data.get("first_name"),
+            last_name=validated_data.get("last_name"),
             phone=validated_data.get("phone"),
             password=validated_data["password"],
         )
 
-       
         user.role = User.CLIENT
         user.save()
 
         return user
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    """
-    Serializer for viewing or updating user profile.
-    """
-
-    class Meta:
-        model = User
-        fields = ("id", "username", "email", "phone", "role")
-        read_only_fields = ("role",)
-
-
