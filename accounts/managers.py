1990d69956c273ae659906a9006159350cb96ffd
Eugene <eugenemisiko438@gmail.com>
2026-03-02 11:48:36 +0300

    modifying themanagers.py in accounts

--- accounts/managers.py
+++ accounts/managers.py
@@ -2,21 +2,30 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+
+    def create_user(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email is required")
-        User = email
-        if User.objects.filter(email=email).exists():
-            raise ValueError("Email already taken")
+
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+
+        if self.model.objects.filter(email=email).exists():
+            raise ValueError("Email already taken")
+
+        user = self.model(
+            username=username,
+            email=email,
+            **extra_fields
+        )
+
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
 
         return user
-    
-    def create_superuser(self, email, password, **extra_fields):
+
+    def create_superuser(self, username, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
-        return self.create_user(email, password, **extra_fields)
+
+        return self.create_user(username, email, password, **extra_fields)
