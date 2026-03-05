2302b30d7fc25424ecff25774583ca1b85e9715d
Eugene <eugenemisiko438@gmail.com>
2026-03-05 08:02:33 +0300

    serializing the mpesa model

--- payments/serializers.py
+++ payments/serializers.py
@@ -4,10 +4,19 @@ from .models import MpesaRequest, MpesaResponse
 class MpesaRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = MpesaRequest
-        fields = "__all__"
-
+        fields = '__all__'
+        
+    def to_represetation(self, instance):
+        ret = super().to_representation(instance)    
+        ret['amount'] = str(instance.amount)
+        return ret
+    
 class MpesaResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = MpesaResponse
-        fields = "__all__"
+        fields = '__all__'
+        
+    def to_represetation(self, instance):
+        ret = super().to_representation(instance)    
+        return ret
 
