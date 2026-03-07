b27baea3c5cc8625b3c7b04948d281ee9d0ad638
Eugene <eugenemisiko438@gmail.com>
2026-03-07 17:16:13 +0300

    create the model for the OrderItem

--- orders/models.py
+++ orders/models.py
@@ -76,4 +76,28 @@ class Invoice(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.invoice_number        
+        return self.invoice_number  
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        "Order",
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+    product_name = models.CharField(max_length=200)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True)
+    def save(self, *args, **kwargs):
+        self.total_price = self.quantity * self.unit_price
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.product_name} ({self.quantity})"          
