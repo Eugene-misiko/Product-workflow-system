a7680b42941fcf563c7dd00b4e308d54540b6634
Eugene <eugenemisiko438@gmail.com>
2026-03-07 17:17:47 +0300

    create the signals for the creation of the invoice

---
+++ orders/signals.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from decimal import Decimal
+import uuid
+
+from .models import Order, Invoice
+
+
+@receiver(post_save, sender=Order)
+def create_invoice(sender, instance, created, **kwargs):
+
+    if created:
+
+        total = instance.total_price
+
+        deposit = total * Decimal("0.70")
+
+        balance = total - deposit
+
+        Invoice.objects.create(
+            order=instance,
+            invoice_number=f"INV-{uuid.uuid4().hex[:8].upper()}",
+            total_amount=total,
+            deposit_amount=deposit,
+            balance_due=balance,
+        )
