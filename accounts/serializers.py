ced44a6303f41e77ae71e19214d3930e499f1fac
Eugene <eugenemisiko438@gmail.com>
2026-03-11 18:24:16 +0300

    update the serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,13 +3,16 @@ from .models import User
 from django.contrib.auth.password_validation import validate_password
 
 class RegisterSerializer(serializers.ModelSerializer):
-    
+
+    """
+    Serializer used for registering a new user.
+    """
+
     password = serializers.CharField(
         write_only=True,
         required=True,
         validators=[validate_password]
     )
-
     class Meta:
         model = User
         fields = [
@@ -22,13 +25,12 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
+            first_name=validated_data["first_name"],
             email=validated_data["email"],
             phone=validated_data.get("phone"),
-            first_name=validated_data.get("first_name"),
             last_name=validated_data.get("last_name"),
             password=validated_data["password"],
         )
-
         user.role = User.CLIENT
         user.save()
 
@@ -44,5 +46,3 @@ class UserProfileSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "first_name", "email", "phone", "role")
         read_only_fields = ("role",)
-
-
