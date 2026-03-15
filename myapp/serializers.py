ff1f38b981d438e7889f09ec01e4e80c782c3934
Eugene <eugenemisiko438@gmail.com>
2026-03-15 13:00:02 +0300

    create the serializers for the product field

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,17 +1,23 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Product, Category, ProductField
 
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
+    category_name = serializers.CharField(source="category.name", read_only=True)
+
     class Meta:
         model = Product
         fields = "__all__"
-        
+
     def get_image(self, obj):
         if obj.image:
             return obj.image.url
-        return None   
+        return None
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"             
+        fields = "__all__"  
+class ProductFieldSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductField
+        fields = "__all__"                   
