a5e8f1345128b4e14f3ed58493b7c71f17312f4a
Eugene <eugenemisiko438@gmail.com>
2026-03-15 13:34:18 +0300

    update the order item model

--- orders/models.py
+++ orders/models.py
@@ -27,7 +27,21 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} - {self.user.username}"
-    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
+    def save(self, *args, **kwargs):
+        self.unit_price = self.product.price
+        self.subtotal = self.unit_price * self.quantity
+        super().save(*args, **kwargs)    
            
 class Invoice(models.Model):
     STATUS_CHOICES = [
