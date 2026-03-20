602ef41ca1955e5e8a8ae77907f7e9563ffb0e10
Eugene <eugenemisiko438@gmail.com>
2026-03-20 14:26:46 +0300

    create the user detail serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -38,3 +38,22 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'website', 'linkedin',
             'notification_preferences'
         ]        
+        
+class UserDetailSerializer(serializers.ModelSerializer):
+    """
+    Detailed user serializer with profile
+    """
+    profile = UserProfileSerializer(read_only=True)
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+    company_name = serializers.CharField(source='company.name', read_only=True)
+    
+    class Meta:
+        model = User
+        fields = [
+            'id', 'email', 'first_name', 'last_name', 'full_name',
+            'role', 'avatar', 'phone', 'address',
+            'company', 'company_name',
+            'email_verified', 'created_at', 'updated_at',
+            'profile'
+        ]
+        read_only_fields = ['id', 'email', 'role', 'company', 'email_verified', 'created_at']
