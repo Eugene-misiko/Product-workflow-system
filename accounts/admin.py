c1c1897da9ff88abf5d6703468a01e1a1cb1d50d
Eugene <eugenemisiko438@gmail.com>
2026-03-20 16:23:01 +0300

    create a new app companies that handles the different companies

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User,UserProfile,Invitation, PasswordResetToken
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Invitation)
+admin.site.register(PasswordResetToken)
+
 
