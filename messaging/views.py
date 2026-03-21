4accc4dcbb36ef775a1f8fd7e56e248463e551b7
Eugene <eugenemisiko438@gmail.com>
2026-03-21 19:39:15 +0300

    create the converstion message viewset,convesation messageview and message viewset

--- messaging/views.py
+++ messaging/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
-
+from rest_framework import generics, viewsets, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from .models import Conversation, Message
+from .serializers import (
+    MessageSerializer, ConversationSerializer,
+    CreateMessageSerializer, StartConversationSerializer
+)
 # Create your views here.
+class ConversationViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ConversationSerializer
+    
+    def get_queryset(self):
+        return Conversation.objects.filter(
+            participants=self.request.user
+        ).order_by('-last_message_at')
+
+
+class MessageViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = MessageSerializer
+    
+    def get_queryset(self):
+        return Message.objects.filter(
+            conversation__participants=self.request.user
+        ).order_by('-created_at')
+
+
+class ConversationMessagesView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = MessageSerializer
+    
+    def get_queryset(self):
+        conversation = get_object_or_404(
+            Conversation,
+            pk=self.kwargs['pk'],
+            participants=self.request.user
+        )
+        return Message.objects.filter(conversation=conversation).order_by('created_at')
