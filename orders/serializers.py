7125d54bffa7fc2799408451f43e808e7f24e961
Eugene <eugenemisiko438@gmail.com>
2026-03-21 15:03:25 +0300

    create the create orde serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -75,4 +75,31 @@ class OrderDetailSerializer(serializers.ModelSerializer):
             'items', 'status_history',
         ]
 
+class CreateOrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+    
+    class Meta:
+        model = Order
+        fields = [
+            'items', 'needs_design', 'design_description',
+            'client_files', 'description', 'priority'
+        ]
+    
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        
+        order = Order.objects.create(**validated_data)
+        
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+        
+        OrderStatusHistory.objects.create(
+            order=order,
+            old_status='',
+            new_status=Order.STATUS_PENDING,
+            note='Order created'
+        )
+        
+        return order
+
 
