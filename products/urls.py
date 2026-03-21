8456a27c6a321205d6c687b53d62610cf79c46d8
Eugene <eugenemisiko438@gmail.com>
2026-03-21 14:31:25 +0300

    add the urls for the products

---
+++ products/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+app_name = 'products'
+
+urlpatterns = [
+
+    # CATEGORIES
+
+    path('categories/', views.CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('categories/create/', views.CreateCategoryView.as_view(), name='category_create'),
+    path('categories/<int:pk>/update/', views.UpdateCategoryView.as_view(), name='category_update'),
+    path('categories/<int:pk>/delete/', views.DeleteCategoryView.as_view(), name='category_delete'),
+    # PRODUCTS
+    path('products/', views.ProductListView.as_view(), name='product_list'),
+    path('products/featured/', views.FeaturedProductsView.as_view(), name='featured_products'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', views.CreateProductView.as_view(), name='product_create'),
+    path('products/<int:pk>/update/', views.UpdateProductView.as_view(), name='product_update'),
+    path('products/<int:pk>/delete/', views.DeleteProductView.as_view(), name='product_delete'),
+    # PUBLIC (No Auth)
+    path('public/products/', views.PublicProductListView.as_view(), name='public_products'),
+    path('public/categories/', views.PublicCategoryListView.as_view(), name='public_categories'),
+]
