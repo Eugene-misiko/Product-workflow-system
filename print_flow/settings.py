daeea6df62edeadc628547477224923b8afc6f97
Eugene <eugenemisiko438@gmail.com>
2026-03-25 10:34:11 +0300

    update requirements.txt

--- print_flow/settings.py
+++ print_flow/settings.py
@@ -68,13 +68,14 @@ TAILWIND_APP_NAME = 'theme'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 FRONTEND_URL = config("FRONTEND_URL")
