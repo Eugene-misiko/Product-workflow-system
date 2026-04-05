3a9557f2dc1e00e0f980829754205ae7a6382ef1
Eugene <eugenemisiko438@gmail.com>
2026-04-05 03:04:20 +0300

    update the company views

--- companies/views.py
+++ companies/views.py
@@ -7,7 +7,7 @@ Company management views including:
 from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from django.shortcuts import get_object_or_404
 from django.db.models import Sum, Count
 from django.utils import timezone
