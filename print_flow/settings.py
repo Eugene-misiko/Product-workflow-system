d0ee8984f51f0f447d237407869d9f03e57b19b6
Eugene <eugenemisiko438@gmail.com>
2026-04-22 13:55:20 +0300

    update the settings

--- print_flow/settings.py
+++ print_flow/settings.py
@@ -73,8 +73,8 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'companies.middleware.TenantMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'companies.middleware.TenantMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
